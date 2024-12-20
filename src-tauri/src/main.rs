09ff2e209f4e97226d248f5da14e9de82b5dd3c3
04041b <longtrip.com@gmail.com>
2024-12-20 01:22:38 -0800

    adding sentry to debug mobile

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    let _guard = sentry::init(("https://bffc3804b28058c983a12fdce3406e0a@o1298580.ingest.us.sentry.io/4508499734757376", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+      }));
+      
     app_lib::run();
 }
